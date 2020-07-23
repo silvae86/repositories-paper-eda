14ffd45a697d3fd9db9dbc0b64491e94cd073728
João Rocha da Silva <joaorosilva@gmail.com>
2020-07-23 22:27:17 +0100

    tfidf done

--- Sources/Dataverse Comparative Review/Similarities.R
+++ Sources/Dataverse Comparative Review/Similarities.R
@@ -10,28 +10,30 @@ prep_fun = function(x) {
   x = str_replace_all(x, "[^[:alnum:]]", " ")
   # collapse multiple spaces
   str_replace_all(x, "\\s+", " ")
+  str_replace_all(x, "allow", " ")
+  str_replace_all(x, "ability", " ")
+  str_replace_all(x, "support", " ")
 }
 
 pacman::p_load(readxl,data.table,stringr,futile.options,tm,stats,lsa,dplyr)
 
-threshold <- 0.2
+threshold <- 0.10
 
 Comparative <- read_excel("Comparative.xlsx", 
                           sheet = "Cross", col_names = FALSE)
 
-rda_parameters <- transpose(Comparative[1:2,])
+rda_parameters <- transpose(Comparative[1:4,])
 rda_parameters <- na.omit(rda_parameters)
-rda_weights <- transpose(Comparative[3:4,])
-rda_weights <- na.omit(rda_weights)
-colnames(rda_weights) <- c("feature", "weight")
-
+colnames(rda_parameters) <- c("category", "feature", "description", "weight")
+rda_parameters$"description" <- paste(rda_parameters$"feature", rda_parameters$"description")
 rownames(rda_parameters)<- c(1:nrow(rda_parameters))
-colnames(rda_parameters) <- c("1", "2")
+
 dataverse_parameters <- Comparative[1:2]
 dataverse_parameters <- na.omit(dataverse_parameters)
-colnames(dataverse_parameters) <- c("1", "2")
+colnames(dataverse_parameters) <- c("category", "description")
+dataverse_parameters$"description" <- paste(dataverse_parameters$"category", dataverse_parameters$"description")
 
-all_parameters <- append(rda_parameters$"2", dataverse_parameters$"2")
+all_parameters <- append(rda_parameters$"description", dataverse_parameters$"description")
 
 all_parameters_corpus <- SimpleCorpus(
   VectorSource(
@@ -49,19 +51,22 @@ all_parameters_Tfidf <- DocumentTermMatrix(
             )
 )
 all_parameters_Matrix <- as.matrix(all_parameters_Tfidf)
+rownames(all_parameters_Matrix) <- all_parameters
 sim <- t(cosine(t(all_parameters_Matrix)))
 
 nCol <- ncol(sim)
 sim <- sim[c(1:nrow(rda_parameters)),c(nrow(rda_parameters):nCol)]
 sim <- sim[,-1]
 
-rownames(sim) <- rda_parameters$"2"
-colnames(sim) <- dataverse_parameters$"2"
+sim <- cbind(rda_parameters$"feature", rda_parameters$"weight", sim)
+colnames(sim) <- append(c("rda_feature", "weight"), dataverse_parameters$"description")
 
 # filter out all those records under the threshold
-sim[sim < threshold] <- ""
+sim[sim < threshold] <- NA
 
-rm(all_parameters, all_parameters_corpus, all_parameters_Tfidf, prep_fun, nCol, Comparative, featureNames, threshold, rda_parameters, dataverse_parameters, all_parameters_Matrix)
+
+  
+rm(all_parameters, all_parameters_corpus, all_parameters_Tfidf, prep_fun, nCol, Comparative, threshold, rda_parameters, dataverse_parameters, all_parameters_Matrix)
 ### Get Repository characteristics
 
 repoFeatures <- read_excel("Comparative.xlsx", 
@@ -71,9 +76,11 @@ repoFeatures <- read_excel("Comparative.xlsx",
 repoFeatures["Categories"] <- NULL
 repoFeatures <- as.matrix(repoFeatures)
 featureNames <- repoFeatures[,1]
-repoFeatures[!(repoFeatures %ilike% "Yes") ] <- 0
-repoFeatures[repoFeatures %ilike% "Yes"] <- 1
+repoFeatures[!(repoFeatures %ilike% "%Yes%") ] <- 0
+repoFeatures[repoFeatures %ilike% "%Yes%"] <- 1
 repoFeatures[,1] <- featureNames
 repoFeatures <- na.omit(repoFeatures)
 repoFeatures <- repoFeatures[,c(1:(ncol(repoFeatures)-5))]
 
+melted <- melt(data.table(sim), na.rm = T, id.vars="rda_feature")
+colnames(melted) <- c("rda_feature", "dataverse_feature", "similarity")
