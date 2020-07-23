cb1357ac96143b35f1589a56d8671a5648bc50e9
João Rocha da Silva <joaorosilva@gmail.com>
2020-07-23 16:41:03 +0100

    Mais matching, falta juntar as tabelas para calcular equivalencia de features

--- Sources/Dataverse Comparative Review/Similarities.R
+++ Sources/Dataverse Comparative Review/Similarities.R
@@ -12,7 +12,7 @@ prep_fun = function(x) {
   str_replace_all(x, "\\s+", " ")
 }
 
-pacman::p_load(readxl,data.table,stringr,futile.options,tm,stats,lsa)
+pacman::p_load(readxl,data.table,stringr,futile.options,tm,stats,lsa,dplyr)
 
 threshold <- 0.2
 
@@ -21,13 +21,15 @@ Comparative <- read_excel("Comparative.xlsx",
 
 rda_parameters <- transpose(Comparative[1:2,])
 rda_parameters <- na.omit(rda_parameters)
+rda_weights <- transpose(Comparative[3:4,])
+rda_weights <- na.omit(rda_weights)
+colnames(rda_weights) <- c("feature", "weight")
+
 rownames(rda_parameters)<- c(1:nrow(rda_parameters))
 colnames(rda_parameters) <- c("1", "2")
 dataverse_parameters <- Comparative[1:2]
 dataverse_parameters <- na.omit(dataverse_parameters)
 colnames(dataverse_parameters) <- c("1", "2")
-
-rm(Comparative)
 
 all_parameters <- append(rda_parameters$"2", dataverse_parameters$"2")
 
@@ -52,7 +54,6 @@ sim <- t(cosine(t(all_parameters_Matrix)))
 nCol <- ncol(sim)
 sim <- sim[c(1:nrow(rda_parameters)),c(nrow(rda_parameters):nCol)]
 sim <- sim[,-1]
-rm(nCol)
 
 rownames(sim) <- rda_parameters$"2"
 colnames(sim) <- dataverse_parameters$"2"
@@ -60,6 +61,19 @@ colnames(sim) <- dataverse_parameters$"2"
 # filter out all those records under the threshold
 sim[sim < threshold] <- ""
 
-write.table(x = sim, file = "similarities.xls", na = "")
+rm(all_parameters, all_parameters_corpus, all_parameters_Tfidf, prep_fun, nCol, Comparative, featureNames, threshold, rda_parameters, dataverse_parameters, all_parameters_Matrix)
+### Get Repository characteristics
 
+repoFeatures <- read_excel("Comparative.xlsx", 
+                          sheet = "Comparative review of data repo", 
+                          col_names = TRUE)
+
+repoFeatures["Categories"] <- NULL
+repoFeatures <- as.matrix(repoFeatures)
+featureNames <- repoFeatures[,1]
+repoFeatures[!(repoFeatures %ilike% "Yes") ] <- 0
+repoFeatures[repoFeatures %ilike% "Yes"] <- 1
+repoFeatures[,1] <- featureNames
+repoFeatures <- na.omit(repoFeatures)
+repoFeatures <- repoFeatures[,c(1:(ncol(repoFeatures)-5))]
 
