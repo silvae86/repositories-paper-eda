5c04ef63305f66c00bfafd1fcfd10b8d3b4cc018
João Rocha da Silva <joaorosilva@gmail.com>
2020-07-27 13:44:10 +0100

    Tabela com features das plataformas abaixo do grafico

--- Sources/Dataverse Comparative Review/analyse.R
+++ Sources/Dataverse Comparative Review/analyse.R
@@ -10,9 +10,10 @@ if (!require(pacman)) {
 # sudo add-apt-repository ppa:c2d4u.team/c2d4u4.0+
 # sudo apt update
 # sudo apt install r-cran-rgl r-cran-rjags r-cran-snow r-cran-ggplot2 r-cran-igraph r-cran-lme4 r-cran-rjava r-cran-devtools r-cran-roxygen2 r-cran-rjava
-pacman::p_load(readxl,data.table,dplyr,lsa,text2vec,stringr,ggplot2,readODS)
+pacman::p_load(readxl,data.table,dplyr,lsa,text2vec,stringr,ggplot2,readODS,grid,gridExtra,reshape2)
 
 source("./read_data.R")
+source("./get_repo_features.R")
 
 # read manually validated table
 sim <- readODS::read_ods(path = "similarities_validated.ods", sheet = "Sheet1", col_names = T, row_names = F)
@@ -47,6 +48,48 @@ merged[,5:ncol(merged)] <- merged[,5:ncol(merged)] * merged$weight
 totals <- colSums(merged[,5:ncol(merged)])
 totals <- as.data.frame(totals)
 totals$platform <- rownames(totals)
-rownames(totals) <- c(1:nrow(totals))
-plot <- ggplot(data=totals)
-View(plot)
+totals <- merge(totals, open_source)
+totals <- merge(totals, free)
+totals <- totals %>%
+          arrange(desc(score))
+colnames(totals) <- c("platform","score", "open_source", "free")
+
+underGraphTableTotals <- t(totals %>%
+                          select(c("open_source", "free")))
+
+# text wrapping for columns of the table under the graph
+
+colwidth <- 6
+
+wrap_text <- function(names, colwidth)
+{
+  names <- sapply(names, function(x) paste(strwrap(x, width = colwidth),  collapse="\n"))
+  names
+}
+
+rownames(underGraphTableTotals) <- wrap_text(c("Código Aberto", "Livre (\"Free To Use\")"), 10)
+colnames(underGraphTableTotals) <- wrap_text(totals$platform, colwidth)
+
+# Create a table
+thm <- ttheme_default(colhead = 
+                        # first unit is the width, and second the height
+                        list(fg_params=list(cex = 0.9), padding=unit.c(unit(5, "mm"), unit(colwidth, "mm"))),
+                      )
+
+tab = tableGrob(underGraphTableTotals, 
+                rows=rownames(underGraphTableTotals),
+                theme=thm,
+                vp = NULL)
+
+# create plot with table underneath
+# margin: unit(c(top, right, bottom, left), units)
+ggplot(totals, aes(x=reorder(platform,-score), y=score))+
+  geom_bar(stat='identity', fill="plum")+
+  ylab("Pontuação de funcionalidades") + 
+  xlab("Plataforma") + 
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
+  geom_text(data=totals, aes(x=reorder(platform,-score), y=score, label = score), vjust=0) +
+  theme(plot.margin=unit(c(0.2,0.2,7,3),"cm")) + 
+  annotation_custom(tab, xmin=1, xmax=8, ymin=-1000, ymax=-23000)
+
+
--- Sources/Dataverse Comparative Review/read_data.R
+++ Sources/Dataverse Comparative Review/read_data.R
@@ -22,3 +22,37 @@ dataverse_parameters <- dataverse_parameters %>%
   filter(category != 'Business model' & description != "Road Maps" & description != "Preservation technology?")
 
 all_parameters <- append(rda_parameters$"description", dataverse_parameters$"description")
+
+select_and_add_platforms_column <- function(df, column_name)
+{
+  df <- df %>% select(c(
+    'Analyze Boston (CKAN)',	
+    'data.world',	
+    'Dryad',	
+    'figshare',	
+    'Harvard Dataverse',	
+    'Mendeley Data',	
+    'Open ICPSR',
+    'Zenodo',	
+    'Open Science Framework',
+    'Dendro'
+  )) 
+  
+  df <- t(df)
+  df <- cbind(rownames(df), df[,1])
+  rownames(df) <- c(1:nrow(df))
+  colnames(df) <- c("platform", column_name)
+  df
+}
+
+open_source <- read_excel("Comparative.xlsx", 
+                          sheet = "Comparative review of data repo", col_names = T) %>%
+                          filter(`Software Features` == "Open Source")
+  
+open_source <- select_and_add_platforms_column(open_source, "open_source")                          
+
+free <- read_excel("Comparative.xlsx", 
+                          sheet = "Comparative review of data repo", col_names = T) %>%
+                          filter(`Software Features` == 'Free To Use')
+
+free <- select_and_add_platforms_column(free, "free")                          
